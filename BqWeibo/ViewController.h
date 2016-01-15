58583ccf3f1323e28efd9bc5541d283d8671148c
LinBq <miniMonster@BEAST.local>
2016-01-15 13:15:49 +0800

    Initial Commit

---
+++ BqWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BqWeibo
+//
+//  Created by LinBq on 16/1/15.
+//  Copyright (c) 2016年 LinBq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BqWeibo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BqWeibo
+//
+//  Created by LinBq on 16/1/15.
+//  Copyright (c) 2016年 LinBq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
